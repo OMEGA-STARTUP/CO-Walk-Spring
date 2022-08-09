c0522a9e96400dec95cec518b08b5e1d82e637d8
CokeLee777 <akak640@kyonggi.ac.kr>
2022-08-09 13:30:20 +0900

    CW-69
    Spring-Boot 프로젝트 파일 세팅

---
+++ src/src/test/java/com/omega/cowalk/CowalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.cowalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
