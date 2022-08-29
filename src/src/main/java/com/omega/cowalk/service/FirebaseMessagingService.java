e50fcb4625d07dceaad77b1b02e9f94b4ffc36ca
CokeLee777 <akak640@kyonggi.ac.kr>
2022-08-29 15:42:13 +0900

    feat(profile): Divide profile and add gitignore
    프로파일별로 분리하였고, firebase sdk 파일을 gitignore에 추가하였습니다.

--- src/src/main/java/com/omega/cowalk/service/FirebaseMessagingService.java
+++ src/src/main/java/com/omega/cowalk/service/FirebaseMessagingService.java
@@ -5,6 +5,7 @@ import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import com.omega.cowalk.domain.dto.NotificationSendRequestDto;
+import com.omega.cowalk.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class FirebaseMessagingService {
 
     private final FirebaseMessaging firebaseMessaging;
+    private final NotificationRepository notificationRepository;
 
     public String sendNotification(NotificationSendRequestDto sendRequestDto, String token) throws FirebaseMessagingException {
         // 알림 정보 세팅
@@ -27,6 +29,12 @@ public class FirebaseMessagingService {
                 .setToken(token)
                 .setNotification(notification)
                 .build();
+
+        notificationRepository.save(com.omega.cowalk.domain.entity.notification.Notification
+                .builder()
+                .title(sendRequestDto.getTitle())
+                .content(sendRequestDto.getContent())
+                .build());
 
         return firebaseMessaging.send(message);
     }
