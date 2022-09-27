57e6e0e5dae00b26099f8b6eeeefdf28792255da
CokeLee777 <akak640@kyonggi.ac.kr>
2022-09-27 15:04:52 +0900

    test(walkhistory): Add walkhistory test logic
    발걸음 히스토리 테스트 로직을 작성하였습니다.

--- src/src/main/java/com/omega/cowalk/domain/dto/WalkHistoryResponseDto.java
+++ src/src/main/java/com/omega/cowalk/domain/dto/WalkHistoryResponseDto.java
@@ -1,12 +1,15 @@
 package com.omega.cowalk.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class WalkHistoryResponseDto {
 
     private LocalDate walkDate;
